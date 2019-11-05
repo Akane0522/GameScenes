66e2b07d7f827d38f450ecd3744caa1deb196b34
akane koizumi <b1644759@planet.kanazawa-it.ac.jp>
2019-11-05 17:19:50 +0900

    ゲーム画面作成

---
+++ GameScene/Assets/Scripts/dog_moving.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dog_moving : MonoBehaviour { 
+
+    public float nowPosi;
+
+    void Start () {
+        nowPosi = this.transform.position.y;
+    }
+
+    void Update () {
+        transform.position = new Vector3(transform.position.x, nowPosi + Mathf.PingPong(Time.time/3, 0.3f), transform.position.z);
+    }
+
+}
